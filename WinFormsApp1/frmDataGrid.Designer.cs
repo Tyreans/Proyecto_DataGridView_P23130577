f62608ec42fd22826bc01756c7f80f54d987e22a
Tyreans <dino.juan1203@gmail.com>
2025-02-17 16:30:03 -0600

    Add project files.

---
+++ WinFormsApp1/frmDataGrid.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace MiPrimerApp2025C
+{
+    partial class frmDataGrid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvDatos = new DataGridView();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            btnGuardar = new Button();
+            saveFileDialogDataGrid = new SaveFileDialog();
+            openFileDialog1 = new OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).BeginInit();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dgvDatos
+            // 
+            dgvDatos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDatos.Dock = DockStyle.Fill;
+            dgvDatos.Location = new Point(3, 3);
+            dgvDatos.Name = "dgvDatos";
+            dgvDatos.RowHeadersWidth = 62;
+            dgvDatos.Size = new Size(1168, 422);
+            dgvDatos.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(dgvDatos, 0, 0);
+            tableLayoutPanel1.Controls.Add(btnGuardar, 0, 1);
+            tableLayoutPanel1.Location = new Point(2, 12);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 86.1167F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 13.8833F));
+            tableLayoutPanel1.Size = new Size(1174, 497);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            btnGuardar.ImageAlign = ContentAlignment.TopCenter;
+            btnGuardar.Location = new Point(3, 445);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(1168, 34);
+            btnGuardar.TabIndex = 1;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = true;
+            btnGuardar.Click += btnGuardar_Click;
+            // 
+            // saveFileDialogDataGrid
+            // 
+            saveFileDialogDataGrid.Filter = "Archivo CSV|*.csv";
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialogDataGrid";
+            openFileDialog1.Filter = "Text files (*.txt)|*.txt|Excel (*.csv*)|*.csv*";
+            openFileDialog1.FileOk += openFileDialog1_FileOk;
+            // 
+            // frmDataGrid
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1178, 521);
+            Controls.Add(tableLayoutPanel1);
+            Name = "frmDataGrid";
+            Text = "DataGridView";
+            Load += frmDataGrid_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dgvDatos;
+        private TableLayoutPanel tableLayoutPanel1;
+        private SaveFileDialog saveFileDialogDataGrid;
+        private OpenFileDialog openFileDialog1;
+        private Button btnGuardar;
+    }
+}
---
+++ WinFormsApp1/frmDataGrid.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiPrimerApp2025C
+{
+    public partial class frmDataGrid : Form
+    {
+        string filePath = "";
+        public frmDataGrid()
+        {
+            InitializeComponent();
+            DialogResult resultado;
+            resultado = openFileDialog1.ShowDialog();
+            if (resultado == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+                try
+                {
+                    string texto = File.ReadAllText(filePath);
+                    string[] excel = texto.Split('\n');
+                    string[] arr = excel[0].Split(',');
+                    int a = arr.Length;
+                    dgvDatos.ColumnCount = a;
+                    dgvDatos.RowCount = excel.Length;
+                    for (int i = 0; i < a; i++) {
+                        dgvDatos.Rows[0].Cells[i].Value = arr[i];
+                    }
+
+                    for (int i = 1; i < (excel.Length)-1; i++) {
+                        arr = excel[i].Split(',');
+                        for (int j = 0; j < a; j++)
+                        {
+                            dgvDatos.Rows[i].Cells[j].Value = arr[j];
+                        }
+                    }
+                
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al abrir el archivo: " + ex.Message);
+                }
+            }
+        }
+
+
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            DialogResult result = saveFileDialogDataGrid.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string filename = saveFileDialogDataGrid.FileName;
+                int total = dgvDatos.Rows.Count - 1;
+                string linea = "";
+                for (int i = 0; i < total; i++)
+                {
+
+                    for (int j = 0; j < dgvDatos.Columns.Count; j++)
+                    {
+                        if (j == dgvDatos.Columns.Count - 1)
+                            linea += dgvDatos.Rows[i].Cells[j].Value.ToString() + "\n";
+                        else
+                            linea += dgvDatos.Rows[i].Cells[j].Value.ToString() + ",";
+                    }
+
+                }
+                File.WriteAllText(filename, linea);
+            }
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void frmDataGrid_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
